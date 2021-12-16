571e7daa9b4f7e60ca2b37c173293b1d47f5ea10
ezhov-da <ezhdenis@yandex.ru>
2021-12-16 23:12:18 +0300

    feat: убрана строка

--- src/main/kotlin/ru/ezhov/featuretoggles/featuretoggle/interfaces/rest/v1/model/FeatureToggleTypesResponseDto.kt
+++ src/main/kotlin/ru/ezhov/featuretoggles/featuretoggle/interfaces/rest/v1/model/FeatureToggleTypesResponseDto.kt
@@ -1,6 +1,5 @@
 package ru.ezhov.featuretoggles.featuretoggle.interfaces.rest.v1.model
 
-
 data class FeatureToggleTypesResponseDto(
         val types: List<FeatureToggleTypeResponseDto> = FeatureToggleTypeResponseDto.values().toList()
 )
